4226cad98ecd2d91fdb8bca5568982ac1836502e
Brent <brent.e.hildebrand@gmail.com>
2017-11-24 18:10:16 -0500

    changed copy in dev section

--- public/bundle.js
+++ public/bundle.js
@@ -25084,7 +25084,7 @@ var Dev = function (_Component) {
     _this.state = {
       sections: [{
         title: "Taxus Street Coffee",
-        text: ["Website/ecommerce with for the Virginia based coffee Company"],
+        text: ["Website/ecommerce for the Virginia based coffee Company"],
         link: "http://www.taxusstreetcoffee.com/",
         bg: "#231f20",
         color: "white",
@@ -25134,7 +25134,7 @@ var Dev = function (_Component) {
         }
       }, {
         title: "Smiley Search Widget",
-        text: ["A simple UI for directly comparing two fonts. It throws an error if a user tries to use a font that is not on their computer."],
+        text: ["Animated search input widget."],
         link: "https://codepen.io/hil/full/GNrYow/",
         bg: "#dadada",
         color: "black",
--- src/components/Dev.js
+++ src/components/Dev.js
@@ -8,7 +8,7 @@ export default class Dev extends Component {
     this.state = {
       sections: [{
         title: "Taxus Street Coffee",
-        text: ["Website/ecommerce with for the Virginia based coffee Company"],
+        text: ["Website/ecommerce for the Virginia based coffee Company"],
         link: "http://www.taxusstreetcoffee.com/",
         bg: "#231f20",
         color: "white",
@@ -61,7 +61,7 @@ export default class Dev extends Component {
         }
       }, {
         title: "Smiley Search Widget",
-        text: ["A simple UI for directly comparing two fonts. It throws an error if a user tries to use a font that is not on their computer."],
+        text: ["Animated search input widget."],
         link: "https://codepen.io/hil/full/GNrYow/",
         bg: "#dadada",
         color: "black",
